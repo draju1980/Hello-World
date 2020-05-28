ff928a950b32ab71772cda4bfeab6fcdb2d15eec
Divakar  M <draju@outlook.com>
2020-05-28 13:54:19 +0000

    tic_tac

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -45,9 +45,9 @@ import random
 
 def choose_first():
     if random.randint(0, 1) == 0:
-        return 'Player 2'
-    else:
         return 'Player 1'
+    else:
+        return 'Player 2'
 
 
 def space_check(board, position):
