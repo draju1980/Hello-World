b72354f84d0ef637f6cef67f685fe8b38707f4f6
Divakar  M <draju@outlook.com>
2020-06-26 10:05:14 +0000

    regular_xpression_find_num.py

---
+++ file_unzip.py
@@ -0,0 +1,5 @@
+import shutil
+shutil.unpack_archive('unzip_me_for_instructions.zip','','zip')
+with open('extracted_content/Instructions.txt') as f:
+    content = f.read()
+    print(content)
---
+++ regular_xpression_find_num.py
@@ -0,0 +1,24 @@
+import re
+#pattern = r'\d{3}-\d{3}-\d{4}'
+def search(file,pattern= r'\d{3}-\d{3}-\d{4}'):
+    f = open(file,'r')
+    text = f.read()
+    
+    if re.search(pattern,text):
+        return re.search(pattern,text)
+    else:
+        return ''
+
+
+import os
+results = []
+for folder , sub_folders , files in os.walk(os.getcwd()+"\\extracted_content"):
+    
+    for f in files:
+        full_path = folder+'\\'+f
+         
+        results.append(search(full_path))
+
+for r in results:
+    if r != '':
+        print(r.group())
